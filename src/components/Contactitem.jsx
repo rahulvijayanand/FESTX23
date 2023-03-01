2525e2f9145ee2089bf878634f8eaaea4a39688d
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-02 00:21:45 +0530

    Update Contactitem.jsx

--- src/components/Contactitem.jsx
+++ src/components/Contactitem.jsx
@@ -4,7 +4,7 @@ import {FaUser} from "react-icons/fa"
 const Contactitem = (props) => {
   return (
     <div className='text-white'>
-      <div className="mx-auto flex flex-col h-[275px] w-[275px] md:h-[400] md:w-[400] border shadow-[0px_0px_40px] shadow-cyan-400  my-10 items-center bg-center">
+      <div className="mx-auto flex flex-col h-[275px] w-[275px] md:h-[350px] md:w-[350px] border shadow-[0px_0px_40px] shadow-cyan-400  my-10 items-center bg-center">
         <h1 className='text-xl md:text-2xl mt-5 md:mt-10 font-semibold'>{props.heading}</h1>
         <div className='my-10 md:my-14 text-xl leading-loose md:text-2xl md:leading-loose '>
         <p className='flex flex-row items-center gap-5'><FaUser/>{props.name1}</p>
