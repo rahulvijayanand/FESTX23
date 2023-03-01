8db4578614e41715ddaf1bf43fb3e84d6cb56c18
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-02 00:25:39 +0530

    Update Contactitem.jsx

--- src/components/Contactitem.jsx
+++ src/components/Contactitem.jsx
@@ -4,9 +4,9 @@ import {FaUser} from "react-icons/fa"
 const Contactitem = (props) => {
   return (
     <div className='text-white'>
-      <div className="mx-auto flex flex-col h-[275px] w-[275px] md:h-[350px] md:w-[350px] border shadow-[0px_0px_40px] shadow-cyan-400  my-10 items-center bg-center">
-        <h1 className='text-xl md:text-2xl mt-5 md:mt-10 font-semibold'>{props.heading}</h1>
-        <div className='my-10 md:my-14 text-xl leading-loose md:text-2xl md:leading-loose '>
+      <div className="mx-auto flex flex-col h-[275px] w-[275px] md:h-[325px] md:w-[325px] border shadow-[0px_0px_40px] shadow-cyan-400  my-10 items-center bg-center">
+        <h1 className='text-xl mt-5 md:mt-10 font-semibold'>{props.heading}</h1>
+        <div className='my-10 md:my-14 text-xl leading-loose md:leading-loose '>
         <p className='flex flex-row items-center gap-5'><FaUser/>{props.name1}</p>
         <p className='flex flex-row items-center gap-5'><ImMobile/>{props.mob1}</p>
         <p className='flex flex-row items-center gap-5'><FaUser/>{props.name2}</p>
