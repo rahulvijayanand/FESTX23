dd7c3df253e44f6b60107c2d206d9fe0586e2842
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-02 00:12:32 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,13 +5,13 @@ const Contact = () => {
     <div className='text-white mt-40'>
       <h1 className='text-center py-5 text-4xl md:text-6xl font-cinzel'>CONTACT</h1>
       <div className="grid place-items-center lg:grid-cols-2 ">
-        <Contactitem heading="Staff Co-Ordinators" name2="Mrs. NIRMALA J" mob2="+91 73973 73234" name1="Mr. NARESH KUMAR S" mob1="+91 98416 53088"/>
-        <Contactitem heading="Student Co-Ordinators" name2="AJAI" mob2="+91 89256 31079" name1="LOKESWARI" mob1="+91 73391 12232"/>
+        <Contactitem heading="Staff Co-Ordinators" name2="Mrs. NIRMALA J" mob2="73973 73234" name1="Mr. NARESH KUMAR S" mob1="98416 53088"/>
+        <Contactitem heading="Student Co-Ordinators" name2="AJAI" mob2="89256 31079" name1="LOKESWARI" mob1="73391 12232"/>
       </div>
       <h1 className='text-center mt-10 text-4xl'>EVENT CO-ORDINATORS</h1>
       <div className="sm:grid md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 p-5 gap-10 place-items-center">
         <Contactitem heading="HACKAHOLIC" name1="AVANTHIKA" mob1="93443 43670" name2="SUJAN" mob2="89393 41756"/>
-        <Contactitem heading="BIZ BAZAAR" name1="AVULA NAGA SANJAY" mob1="+91 89395 55757" name2="POOJA" mob2="99433 95924"/>
+        <Contactitem heading="BIZ BAZAAR" name1="AVULA NAGA SANJAY" mob1="89395 55757" name2="POOJA" mob2="80723 41630"/>
         <Contactitem heading="TECH TALE" name1="AFRIN" mob1="70106 74056" name2="KAVIN PRABHAKAR" mob2="75980 96947"/>
         <Contactitem heading="BLOODY BUGGERS" name1="KRISHNA KUMAR" mob1="77080 77528" name2="HARITHAA" mob2="99622 91235"/>
         <Contactitem heading="LIGHTS ON" name1="JAI VIGNESH" mob1="88256 45904" name2="BALAJI" mob2="86678 58430"/>
