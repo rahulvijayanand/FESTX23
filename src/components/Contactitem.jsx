0b7ee6ca5e8625f9399d0ce8ef8cd6eaf02138cf
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-02 00:31:00 +0530

    contact

--- src/components/Contactitem.jsx
+++ src/components/Contactitem.jsx
@@ -6,7 +6,7 @@ const Contactitem = (props) => {
     <div className='text-white'>
       <div className="mx-auto flex flex-col h-[275px] w-[275px] md:h-[325px] md:w-[325px] border shadow-[0px_0px_40px] shadow-cyan-400  my-10 items-center bg-center">
         <h1 className='text-xl mt-5 md:mt-10 font-semibold'>{props.heading}</h1>
-        <div className='my-10 md:my-14 text-xl leading-loose md:leading-loose '>
+        <div className='my-10 md:my-14 text-base leading-loose md:leading-loose '>
         <p className='flex flex-row items-center gap-5'><FaUser/>{props.name1}</p>
         <p className='flex flex-row items-center gap-5'><ImMobile/>{props.mob1}</p>
         <p className='flex flex-row items-center gap-5'><FaUser/>{props.name2}</p>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,13 +10,13 @@ const Contact = () => {
       </div>
       <h1 className='text-center mt-10 text-2xl lg:text-3xl'>EVENT CO-ORDINATORS</h1>
       <div className="sm:grid md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 p-5 gap-10 place-items-center">
-        <Contactitem heading="HACKAHOLIC" name1="AVANTHIKA" mob1="93443 43670" name2="SUJAN" mob2="89393 41756"/>
-        <Contactitem heading="BIZ BAZAAR" name1="AVULA NAGA SANJAY" mob1="89395 55757" name2="POOJA" mob2="80723 41630"/>
-        <Contactitem heading="TECH TALE" name1="AFRIN" mob1="70106 74056" name2="KAVIN PRABHAKAR" mob2="75980 96947"/>
-        <Contactitem heading="BLOODY BUGGERS" name1="KRISHNA KUMAR" mob1="77080 77528" name2="HARITHAA" mob2="99622 91235"/>
-        <Contactitem heading="LIGHTS ON" name1="JAI VIGNESH" mob1="88256 45904" name2="BALAJI" mob2="86678 58430"/>
-        <Contactitem heading="PITCH UP" name1="HARIBABU" mob1="86680 70272" name2="SREE LAKSHMI" mob2="70940 81674"/>
-        <Contactitem heading="PARAGON QUEST" name1="SURETHA" mob1="93602 51179" name2="DIVYESH" mob2="89390 20427"/>
+        <Contactitem heading="HACKAHOLIC" name1="AVANTHIKA P V" mob1="93443 43670" name2="SUJAN B" mob2="89393 41756"/>
+        <Contactitem heading="BIZ BAZAAR" name1="AVULA NAGA SANJAY" mob1="89395 55757" name2="POOJA S" mob2="80723 41630"/>
+        <Contactitem heading="TECH TALE" name1="AFRIN M" mob1="70106 74056" name2="KAVIN PRABHAKAR RA" mob2="75980 96947"/>
+        <Contactitem heading="BLOODY BUGGERS" name1="KRISHNA KUMAR B" mob1="77080 77528" name2="HARITHAA S" mob2="99622 91235"/>
+        <Contactitem heading="LIGHTS ON" name1="JAI VIGNESH R" mob1="88256 45904" name2="BALAJI A" mob2="86678 58430"/>
+        <Contactitem heading="PITCH UP" name1="HARIBABU N" mob1="86680 70272" name2="SREE LAKSHMI G" mob2="70940 81674"/>
+        <Contactitem heading="PARAGON QUEST" name1="SURETHA S" mob1="93602 51179" name2="DIVYESH S" mob2="89390 20427"/>
       </div>
     </div>
   )
