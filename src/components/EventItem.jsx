997db2ed530cf1f9b47981c36c4f722409952d0e
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-02 07:07:48 +0530

    Update EventItem.jsx

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -5,10 +5,10 @@ export default function EventItem(props) {
   const navigate=useNavigate();
 
   return (
-    <div className="flex flex-col w-[275px] h-[275px] md:h-[400px] md:w-[400px] border shadow-[0px_0px_40px] shadow-cyan-400 p-5 mt-10 bg-transparent bg-cover	items-center ">
+    <div className="flex flex-col w-[275px] h-[275px] md:h-[300px] md:w-[300px] border shadow-[0px_0px_40px] shadow-cyan-400 p-5 mt-10 bg-transparent bg-cover	items-center ">
         <div className="flex flex-col items-center ">
       <p className="p-2 text-center text-xl md:text-3xl font-semibold">{props.title1}</p>
-      <p className="text-white mt-7 md:mt-10 text-6xl md:text-9xl">{props.title2}</p></div>
+      <p className="text-white mt-7 md:mt-10 text-6xl md:text-6xl">{props.title2}</p></div>
       <button onClick={() => {navigate(props.title3)}} className="bg-transparent hover:bg-cyan-500 text-white mt-10 font-semibold text-xl md:text-2xl  w-40 md:w-44 h-12 hover:text-white py-2 px-4 border border-white-500 hover:border-transparent rounded">
         View Details
       </button>
