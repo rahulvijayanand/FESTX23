26bb2c6e4581490e93273c08743e833c3aaa2fa5
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-05 18:23:42 +0530

    Update EventDetails.jsx

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -114,7 +114,7 @@ export default function EventDetails(props) {
       <div>
         <Contact contact={props.event.contact} />
       </div>
-      <div className="uppercase bg-transparent border w-[150px] rounded-md font-medium sm:text-2xl md:text-2xl my-6 mx-auto py-3 text-center">
+      <div className="uppercase bg-transparent border w-[150px] rounded-md font-medium sm:text-2xl md:text-2xl my-6 mx-auto py-3 text-center hover:bg-cyan-500">
         <a href="//forms.gle/dmdhQdMV3vJL1ezg7" target="_blank" rel="noopener noreferrer">
           REGISTER
         </a>
